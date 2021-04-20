f731c18cbc986eac384773ffb42176414cd70e97
Sla-Vikk <9whiterwhite@gmail.com>
2021-04-20 19:31:04 +0300

    created modules

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import s from './Navbar.module.css';
 
 const Navbar = () => {
-    return <nav className='nav'>
+    return <nav className={s.nav}>
         <ul>
-            <li><a href="">Profile</a></li>
-            <li><a href="">Messages</a></li>
-            <li><a href="">News</a></li>
-            <li><a href="">Music</a></li>
-            <li><a href="">Settings</a></li>
+            <li className={`${s.item} ${s.active}`}><a href="">Profile</a></li>
+            <li className={s.item}><a href="">Messages</a></li>
+            <li className={s.item}><a href="">News</a></li>
+            <li className={s.item}><a href="">Music</a></li>
+            <li className={s.item}><a href="">Settings</a></li>
         </ul>
     </nav>
 }
