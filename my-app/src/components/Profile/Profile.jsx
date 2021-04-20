2a077dfce25756fe30bb2815730fdd062fc3b244
Sla-Vikk <9whiterwhite@gmail.com>
2021-04-20 20:47:37 +0300

    created folders for components

---
+++ my-app/src/components/Profile/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './Profile.module.css';
+import MyPosts from './MyPosts/MyPosts.jsx';
+
+const Profile = () => {
+    return <div className={s.content}>
+        <div>
+            <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="main-img"/>
+        </div>
+        <div>avatar + description</div>
+        <MyPosts />
+        </div>
+        }
+
+export default Profile;
