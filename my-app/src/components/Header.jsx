a9eca760deaae854b34e3345896d1163cbdbd04e
Sla-Vikk <9whiterwhite@gmail.com>
2021-04-20 19:30:32 +0300

    created modules

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
-    return <header className='header'>
+    return <header className={s.header}>
         <img src="https://codesign.com.bd/conversations/content/images/2020/03/Sprint-logo-design-Codesign-agency.png" alt="logo"/>
     </header>
 }
