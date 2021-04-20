d5c04948919bd811ba9efcf52f872494691379f8
Sla-Vikk <9whiterwhite@gmail.com>
2021-04-20 19:31:15 +0300

    created modules

--- my-app/src/components/Profile.jsx
+++ my-app/src/components/Profile.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
-    return <div className='content'><div>
+    return <div className={s.content}><div>
         <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="main-img"/>
     </div>
         <div>avatar + description</div>
         <div>
-            <div>my posst</div>
-            <div>new post</div>
+            <div >my posst</div>
+            <div >new post</div>
         </div>
-        <div>post1</div>
-
+        <div className={s.item}>post1</div>
+        <div className={s.item}>post2</div>
     </div>
 }
 
