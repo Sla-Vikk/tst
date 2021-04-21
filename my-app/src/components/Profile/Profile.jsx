84a18fc5760ad5fc6cfee6fe92fcfa9e014ccb78
Sla-Vikk <9whiterwhite@gmail.com>
2021-04-21 14:40:23 +0300

    restructure posts block (component)

--- my-app/src/components/Profile/Profile.jsx
+++ my-app/src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts.jsx';
 
 const Profile = () => {
-    return <div className={s.content}>
+    return <div>
         <div>
             <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="main-img"/>
         </div>
